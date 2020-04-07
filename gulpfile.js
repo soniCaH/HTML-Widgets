9cccdebf4dc93f5d522833697134be351c90a850
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2020-04-07 22:55:30 +0200

    Preparing weather page.

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,6 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var concat = require('gulp-concat');
 var uglify = require('gulp-terser');
-var optimizejs = require('gulp-optimize-js');
 
 // Styles
 var sass = require('gulp-sass');
@@ -113,11 +112,9 @@ var cleanDist = function (done) {
 // Repeated JavaScript tasks
 var jsTasks = lazypipe()
 	.pipe(header, banner.main, {package: package})
-	// .pipe(optimizejs)
 	.pipe(dest, paths.scripts.output)
 	.pipe(rename, {suffix: '.min'})
 	.pipe(uglify)
-	// .pipe(optimizejs)
 	.pipe(header, banner.main, {package: package})
 	.pipe(dest, paths.scripts.output);
 
