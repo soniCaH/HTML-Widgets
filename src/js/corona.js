5f525f292881e480f7816cbd7d9978579f19dde9
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2020-04-07 16:21:38 +0200

    Display totals.

--- src/js/corona.js
+++ src/js/corona.js
@@ -13,7 +13,32 @@ async function getRecovered() {
     const data = await response.json();
     console.log(data);
 };
+async function getSummary() {
+    const response = await fetch('https://api.covid19api.com/summary');
+    const { Global, Countries } = await response.json();
 
-getConfirmed();
-getDeaths();
-getRecovered();
+    document.getElementById('corona_global--total--confirmed').innerHTML = Global.TotalConfirmed.toLocaleString('nl-BE');
+    document.getElementById('corona_global--total--deaths').innerHTML = Global.TotalDeaths.toLocaleString('nl-BE');
+    document.getElementById('corona_global--total--recovered').innerHTML = Global.TotalRecovered.toLocaleString('nl-BE');
+
+    document.getElementById('corona_global--new--confirmed').innerHTML = Global.NewConfirmed.toLocaleString('nl-BE');
+    document.getElementById('corona_global--new--deaths').innerHTML = Global.NewDeaths.toLocaleString('nl-BE');
+    document.getElementById('corona_global--new--recovered').innerHTML = Global.NewRecovered.toLocaleString('nl-BE');
+
+    const Belgium = Countries.find(Country => Country.Country === 'Belgium');
+
+    document.getElementById('corona_belgium--total--confirmed').innerHTML = Belgium.TotalConfirmed.toLocaleString('nl-BE');
+    document.getElementById('corona_belgium--total--deaths').innerHTML = Belgium.TotalDeaths.toLocaleString('nl-BE');
+    document.getElementById('corona_belgium--total--recovered').innerHTML = Belgium.TotalRecovered.toLocaleString('nl-BE');
+
+    document.getElementById('corona_belgium--new--confirmed').innerHTML = Belgium.NewConfirmed.toLocaleString('nl-BE');
+    document.getElementById('corona_belgium--new--deaths').innerHTML = Belgium.NewDeaths.toLocaleString('nl-BE');
+    document.getElementById('corona_belgium--new--recovered').innerHTML = Belgium.NewRecovered.toLocaleString('nl-BE');
+
+    console.log({Global, Countries});
+}
+
+// getConfirmed();
+// getDeaths();
+// getRecovered();
+getSummary();
