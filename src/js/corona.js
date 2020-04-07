657bc26e2cfc12fcaf7c591b7cac48ee5b5099c4
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2020-04-07 16:00:57 +0200

    Fetch raw data.

--- src/js/corona.js
+++ src/js/corona.js
@@ -1,5 +1,19 @@
-async function getDeaths() {
+async function getConfirmed() {
     const response = await fetch('https://api.covid19api.com/country/belgium/status/confirmed/live');
     const data = await response.json();
     console.log(data);
-}
+};
+async function getDeaths() {
+    const response = await fetch('https://api.covid19api.com/country/belgium/status/deaths/live');
+    const data = await response.json();
+    console.log(data);
+};
+async function getRecovered() {
+    const response = await fetch('https://api.covid19api.com/country/belgium/status/recovered/live');
+    const data = await response.json();
+    console.log(data);
+};
+
+getConfirmed();
+getDeaths();
+getRecovered();
